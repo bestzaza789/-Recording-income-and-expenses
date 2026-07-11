674ffa6436c0bf71fde38cfd0cf2d9be51368a2f
bestzaza789 <146432574+bestzaza789@users.noreply.github.com>
2026-07-11 23:50:02 +0700

    feat: generate due recurring transactions at startup

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { seedDefaultsIfEmpty } from './db/db'
+import { generateDueRecurring } from './db/recurringEngine'
 
-seedDefaultsIfEmpty().finally(() => {
-  createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  )
-})
+seedDefaultsIfEmpty()
+  .then(() => generateDueRecurring())
+  .catch(() => {
+    // generation failure must never block app startup
+  })
+  .finally(() => {
+    createRoot(document.getElementById('root')!).render(
+      <StrictMode>
+        <App />
+      </StrictMode>,
+    )
+  })
